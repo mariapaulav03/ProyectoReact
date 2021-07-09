8f6435a51c7dbff140d89ec93878e3c57f7c84dd
mariapaulav03 <14mariapaula09@gmail.com>
2021-07-09 07:13:54 -0500

    Se agrego cambios a la ruta del repositorio

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 
 function App(){
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div className="App">
                 <Switch>
                 <Route exact path="/adop">
